13f26a749d1fa03d8f414074f1ef5aab02c0bbd2
Luna <bedgowns@gmail.com>
2023-05-31 19:18:45 -0700

    database connection for api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "12012003",
+      // database: "voyage",
+      entities: [],
+      synchronize: process.env.NODE_ENV === "development",
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
