90fbf55c8642fe3ab54df338ca9e3abefa31214c
Luna <bedgowns@gmail.com>
2023-05-31 18:23:27 -0700

    added api app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  // eslint-disable-next-line max-len
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
   settings: {
