5b876f3aeff30f7674d530ef61380b764f4b6e4f
Luna <bedgowns@gmail.com>
2023-06-05 11:45:06 -0700

    Refactor baseUrl field to include apiPort dynamically

--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -1,24 +1,28 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => ({
-  apiPort: parseInt(process.env.API_PORT, 10) || 3001,
-  baseUrl:
-    process.env.NODE_ENV === "production"
-      ? "https://voyage.com"
-      : "http://localhost:" + (parseInt(process.env.API_PORT, 10) || 3001),
-  database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-  },
-  auth: {
-    google: {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+export default () => {
+  const apiPort = parseInt(process.env.API_PORT, 10) || 3001;
+
+  return {
+    apiPort,
+    baseUrl:
+      process.env.NODE_ENV === "production"
+        ? "https://voyage.com"
+        : `http://localhost:${apiPort}`,
+    database: {
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
     },
-    facebook: {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
+    auth: {
+      google: {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      },
+      facebook: {
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+      },
     },
-  },
-});
+  };
+};
